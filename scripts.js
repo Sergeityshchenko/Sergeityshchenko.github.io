6004d9012abdae36cb4bce1a1854e3ef66352651
Sergei Tyshchenko <styshchenko4@gmail.com>
2019-01-21 15:50:57 +0600

    Fixed different bugs

---
+++ scripts.js
@@ -0,0 +1,38 @@
+const navLinks = document.querySelectorAll('nav a');
+
+const articles = document.querySelectorAll('article');
+
+
+
+let activeLink = navLinks[0]; //Home
+
+navLinks.forEach(function(navLink) {
+  
+navLink.addEventListener('click', function(event) {
+    // Deactivate current page
+   
+    activeLink.className = "";
+
+    const urlParts = activeLink.href.split('#');
+ 
+    const afterHashtag = urlParts.pop();
+    
+    const oldArticle = document.getElementById(afterHashtag);
+    
+    oldArticle.className = "";
+
+    // Set new page
+    
+    activeLink = this;
+
+    // Activate new page
+   
+    activeLink.className = "active";
+ 
+   document
+.getElementById(activeLink.href.split('#').pop())
+      .className = "active";
+
+    event.preventDefault();
+  });
+})
